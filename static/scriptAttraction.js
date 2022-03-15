08497d3d72f51e29c5af0d4425678eddabda5814
Emerson Fan <metalraccoon@gmail.com>
2022-03-15 23:00:10 +0800

    finished attraction rwd and little buttons on images

--- static/scriptAttraction.js
+++ static/scriptAttraction.js
@@ -2,23 +2,55 @@
 const morningBt = document.querySelector("#morning");
 const afternoonBt = document.querySelector("#afternoon");
 const currentUrl = window.location.href;
+
+const leftArrow = document.querySelector("#leftArrow");
+const rightArrow = document.querySelector("#rightArrow");
 let cutPosition = currentUrl.indexOf("n/") + 2;
 let currentId = currentUrl.substring(cutPosition);
-console.log(currentId);
+
+async function fetchImgs() {
+  const res = await fetch("/api/attraction/" + currentId);
+  const data = await res.json();
+  const attraction = data.data;
+  let imgs = attraction["images"];
+  return imgs;
+}
 
 async function fetchAttractionById() {
   const res = await fetch("/api/attraction/" + currentId);
   const data = await res.json();
   const attraction = data.data;
   console.log(attraction);
-  const img = document.querySelector("#attractionImgContainer > img");
+  const imgs = document.querySelector("#attractionImgContainer > img");
   const name = document.querySelector(".attractionName > h2");
   const category = document.querySelector(".attractionNameCategory");
   const area = document.querySelector(".attractionNameArea");
   const description = document.querySelector(".attractionBodyDescription");
   const address = document.querySelector(".attractionBodyAddress");
   const transport = document.querySelector(".attractionBodyTransport");
-  img.src = attraction["images"][0];
+  const attactionImgLength = attraction["images"].length;
+  const circleArea = document.querySelector("#circleArea");
+  // let leftRange = 0;
+  // let offSet = 3 * attactionImgLength;
+  // let finalLeftRange = leftRange + offSet;
+
+  // circleArea.style.left = finalLeftRange + "%";
+  imgs.src = attraction["images"][0];
+
+  // 做圖片底下的圈鈕 圖片幾張就做多少顆
+  // 如果按鈕小於或大於幾顆會讓版面跑掉 或許要針對幾顆來做版面屬性調整
+  // 或許可以考慮以三為基數? .circleArea 3顆 = 25% 6顆 =50%?
+  // 不知道該怎置中= =
+  for (let i = 0; i < attactionImgLength; i++) {
+    const button = document.createElement("button");
+    button.classList.add("circleBtGroup");
+    circleArea.append(button);
+  }
+  const circleBtGroup = document.querySelectorAll(".circleBtGroup");
+  console.log(circleBtGroup);
+  //一旦左右鍵被按就要移除這個chosenCircle class然後讓左邊或右邊下一個加上class
+  circleBtGroup[0].classList.add("chosenCircle");
+
   // 這邊要想辦法做輪播
   name.textContent = attraction["name"];
   category.textContent = attraction["category"];
@@ -45,4 +77,18 @@ morningBt.addEventListener("click", function () {
 
 afternoonBt.addEventListener("click", function () {
   changeMoney(2500);
+});
+
+leftArrow.addEventListener("click", async function (e) {
+  e.preventDefault();
+  console.log("hi");
+  let imgs = fetchImgs();
+});
+
+rightArrow.addEventListener("click", async function (e) {
+  e.preventDefault();
+  console.log("hi");
+  let imgs = await fetchImgs();
+  // await due to it's an async func
+  console.log(imgs.length);
 });
