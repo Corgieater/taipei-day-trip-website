0f532211800d9901b2d243414d055b2faeb20d5a
Emerson Fan <metalraccoon@gmail.com>
2022-03-21 22:25:00 +0800

    fix date form padding, border and margin

--- static/scriptSignInBox.js
+++ static/scriptSignInBox.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const wrapForWholeSignInBox = document.querySelector("#wrapForWholeSignInBox");
+const signInOrSignUpBt = document.querySelector("#signInOrSignUpBt");
 const signInBt = document.querySelector("#signInBt");
+const signUpBt = document.querySelector("#signUpBt");
 const closeBt = document.querySelector("#closeBt");
 
 console.log("connected");
 
-signInBt.addEventListener("click", function (e) {
+signInOrSignUpBt.addEventListener("click", function (e) {
   e.preventDefault();
   wrapForWholeSignInBox.classList.remove("hide");
 });
@@ -14,4 +16,14 @@ signInBt.addEventListener("click", function (e) {
 closeBt.addEventListener("click", function (e) {
   e.preventDefault();
   wrapForWholeSignInBox.classList.add("hide");
+});
+
+signInBt.addEventListener("click", function (e) {
+  e.preventDefault();
+});
+
+signUpBt.addEventListener("click", function (e) {
+  e.preventDefault();
+  const signIn = document.querySelector(".signIn");
+  signIn.classList.add("hide");
 });
